a7c1e0d8d33e299a53d146d28ca0b16f5ffcc2ad
karthik65432 <karthik65432@gmail.com>
2021-06-20 11:03:40 +0530

    Editing and new post page created. CSS and JS inherited from bootstrap

--- app.py
+++ app.py
@@ -24,22 +24,44 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/posts', methods = ['GET','POST'])
+@app.route('/posts')
 def posts():
+    all_posts = Blogpost.query.order_by(Blogpost.date_posted)
+    return render_template('posts.html', posts=all_posts)
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = Blogpost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+
+@app.route('/posts/edit/<int:id>', methods = ['GET','POST'])
+def edit(id):
+    post = Blogpost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        post.author = request.form['author']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html',post=post)
+
+@app.route('/posts/new', methods = ['GET','POST'])
+def newpost():
     if request.method == 'POST':
         post_title = request.form['title']
         post_content = request.form['content']
-        new_post = Blogpost(title = post_title, content = post_content)
+        post_author = request.form['author']
+        new_post = Blogpost(title=post_title, content=post_content, author=post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
     else:
-        all_posts = Blogpost.query.order_by(Blogpost.date_posted)
-        return render_template('posts.html', posts=all_posts)
+        return render_template('newpost.html')
 
-@app.route('/home/<string:name>')
-def hello(name):
-    return 'Hello, ' + name
 
 
 if __name__ == '__main__':
